8e06cae9ae1034b76e63e76c8b1796dd9d3748b9
odiwm <ygun2k14@gmail.com>
2023-02-08 18:13:29 +0900

    Signed-off-by: odiwm <ygun2k14@gmail.com>

--- captcha/src/captcha/CaptchaControl.java
+++ captcha/src/captcha/CaptchaControl.java
@@ -19,6 +19,8 @@ import nl.captcha.Captcha;
 @WebServlet("/CaptchaControl")
 public class CaptchaControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
        
     /**
      * @see HttpServlet#HttpServlet()
